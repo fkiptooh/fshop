cd9c702cd28add4873804821c95426a6ce489af3
Felix Kiptoo <fkiptooh@gmail.com>
2024-04-02 23:39:06 +0300

    feat(auth) - login page setup

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,5 +1,58 @@
-export const Login =() => {
+import { useState } from "react";
+import { Button } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  const loginForm = () => (
+    <form>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          placeholder="Enter your email to register"
+          onChange={(e) => setEmail(e.target.value)}
+          autoFocus
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <br />
+      <Button
+        onClick={handleSubmit}
+        type="primary"
+        className="mb-3"
+        block
+        shape="round"
+        icon={<MailOutlined />}
+        disabled={!email && password.length < 6}
+      >
+        Login With Email/Password
+      </Button>
+    </form>
+  );
   return (
-    <div>Login</div>
-  )
-}
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Login</h4>
+          {loginForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
